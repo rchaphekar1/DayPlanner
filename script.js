25951a745cd69eeb471ebd65d752a4c629ca969a
Rahul Chaphekar <rchaphekar1@gmail.com>
2021-01-11 18:13:57 -0600

    Redid rows and columns as table, added local storage

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 $(document).ready(function() {
     var m = moment();
-    var timeArray = ["09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
+    var timeArray = ["09 AM", "10 AM", "11 AM", "12 PM", "01 PM", "02 PM", "03 PM", "04 PM", "05 PM"];
     function dateToday() {
         $("#currentDay").text(m.format('dddd, MMMM Do YYYY'));
     };
+    dateToday();
     
-    var newRow = $("<tr>");
-    var timeCol = $("<td>");
-    var eventCol = $("<td>");
-    var buttonCol = $('<td><button type="button" class="btn btn-primary saveBtn">Save</button></td>');
-    newRow.append(timeCol);
-    newRow.append(eventCol);
-    newRow.append(buttonCol);
     function addRow() {
-        for (var i = 0; i < 9; i++) {
-            eventCol.text(timeArray[i]);
-            $("tbody").append(newRow.clone());
+        for (var i = 0; i < timeArray.length; i++) {
+            var newRow = $("<tr>");
+            var timeCol = $("<td>");
+            var eventCol = $('<td id="event-col" contenteditable="true"></td>');
+            var buttonCol = $('<td><button type="button" class="btn btn-primary saveBtn">Save</button></td>')
+                .attr("value", i);
+            newRow.append(timeCol);
+            newRow.append(eventCol);
+            newRow.append(buttonCol);
+            timeCol.text(timeArray[i]);
+            $("table").append(newRow);
+            if (moment().isSame(timeArray[i])) {
+                buttonCol.addClass("present");
+            } else if (moment().isBefore(timeArray[i])) {
+                buttonCol.addClass("future");
+            } else if (moment().isAfter(timeArray[i])) {
+                buttonCol.addClass("past");
+            };
         };
     };
-    dateToday();
     addRow();
+    
+    var eventInfo;
+    $(".saveBtn").on("click", function(event) {
+        console.log(this.value);
+        event.preventDefault();
+        eventInfo = $("#event-col").text();
+        console.log(eventInfo);
+        localStorage.setItem("eventInfo", JSON.stringify(eventInfo));
+    });
+    eventInfo = JSON.parse(localStorage.getItem("eventInfo"));
+    $("#event-col").text(eventInfo);
 });
