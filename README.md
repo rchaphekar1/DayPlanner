911748aecb41612376cbecf645bb7f35fc311fb0
Rahul Chaphekar <rchaphekar1@gmail.com>
2021-01-12 00:23:51 -0600

    Added comments to js and updated README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # DayPlanner
+
+# Made a day planner dynamically using jquery
+
+![Screenshot](DayPlanner.png?raw=true "Day Planner Screenshot")
+
+https://github.com/rchaphekar1/DayPlanner
+https://rchaphekar1.github.io/DayPlanner/
--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
     var m = moment();
     var timeArray = ["09 AM", "10 AM", "11 AM", "12 PM", "01 PM", "02 PM", "03 PM", "04 PM", "05 PM"];
+    
+    // Function that uses moment.js to get the current date
     function dateToday() {
         $("#currentDay").text(m.format('dddd, MMMM Do YYYY'));
     };
     dateToday();
     
+    // Function to add rows to page dynamically
     function addRow() {
         for (var i = 0; i < timeArray.length; i++) {
             var newRow = $("<tr>");
@@ -18,6 +21,7 @@ $(document).ready(function() {
             newRow.append(buttonCol);
             timeCol.text(timeArray[i]);
             $("table").append(newRow);
+            // Attempted to use Moment methods to compare current time to those listed
             if (moment().isSame(timeArray[i])) {
                 buttonCol.addClass("present");
             } else if (moment().isBefore(timeArray[i])) {
@@ -29,6 +33,7 @@ $(document).ready(function() {
     };
     addRow();
     
+    // Saves to local storage on button click
     var eventInfo;
     $(".saveBtn").on("click", function(event) {
         console.log(this.value);
